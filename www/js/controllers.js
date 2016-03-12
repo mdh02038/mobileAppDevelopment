0a5e8d59d8fd3bbf0453560cd1b24c31c6475d2c
Mark Hummel <mdh@raquette.com>
2016-03-11 22:23:54 -0500

    initial import

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -216,19 +216,29 @@ menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
             $scope.leaders = corporateFactory.query();
       }])
 
-.controller('FavoritesController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate', function ($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate) {
+.controller('FavoritesController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate', '$ionicPopup', '$ionicLoading', '$timeout', function ($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate, $ionicPopup, $ionicLoading, $timeout) {
 
     $scope.baseURL = baseURL;
     $scope.shouldShowDelete = false;
+
+    $ionicLoading.show({
+        template: '<ion-spinner></ion-spinner> Loading...'
+    });
 
     $scope.favorites = favoriteFactory.getFavorites();
 
     $scope.dishes = menuFactory.getDishes().query(
         function (response) {
             $scope.dishes = response;
+            $timeout(function () {
+                $ionicLoading.hide();
+            }, 1000);
         },
         function (response) {
             $scope.message = "Error: " + response.status + " " + response.statusText;
+            $timeout(function () {
+                $ionicLoading.hide();
+            }, 1000);
         });
     console.log($scope.dishes, $scope.favorites);
 
@@ -242,7 +252,29 @@ menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
         favoriteFactory.deleteFromFavorites(index);
         $scope.shouldShowDelete = false;
 
-    }}])
+    }
+    $scope.deleteFavorite = function (index) {
+
+        var confirmPopup = $ionicPopup.confirm({
+            title: 'Confirm Delete',
+            template: 'Are you sure you want to delete this item?'
+        });
+
+        confirmPopup.then(function (res) {
+            if (res) {
+                console.log('Ok to delete');
+                favoriteFactory.deleteFromFavorites(index);
+            } else {
+                console.log('Canceled delete');
+            }
+        });
+
+        $scope.shouldShowDelete = false;
+
+    }
+
+    }])
+
 .filter('favoriteFilter', function () {
     return function (dishes, favorites) {
         var out = [];
